cb7901d85fa86e0d0d665f2c834997a2bc22e90f
James Stone <jamesmstone@users.noreply.github.com>
2016-08-30 13:32:25 +1000

    Add Feedback for feedback 1,2,4

--- Proj1.hs
+++ Proj1.hs
@@ -1,28 +1,23 @@
 module Proj1 (feedback, initialGuess, nextGuess, GameState) where
+import Data.List
 import Card
 
-feedback :: [Card]->[Card]->(Int, Int, Int, Int, Int)
+feedback :: [Card]->[Card]->Feedback
 -- takes a target and a guess (in that order), each represented as a list of Cards, and returns the five feedback numbers, as explained above, as a tuple.
-feedback target guess = ((feedback1 target guess),(feedback2 target guess),(feedback3 target guess),(feedback4 target guess),(feedback5 target guess))
-
-feedback1:: [Card]->[Card]->Int
-feedback1 _ _ = 0
-
-feedback2:: [Card]->[Card]->Int
-feedback2 _ _ = 0
-
-feedback3:: [Card]->[Card]->Int
-feedback3 _ _ = 0
-
-feedback4:: [Card]->[Card]->Int
-feedback4 _ _ = 0
-
-feedback5:: [Card]->[Card]->Int
-feedback5 _ _ = 0
+feedback target guess = (feedback1 ,feedback2 ,feedback3,feedback4,feedback5)
+  where
+  feedback1 = length(target `intersect` guess)
+  feedback2 = length(filter (< (minimum guess)) target)
+  feedback3 = 0
+  feedback4 = length(filter (> (maximum guess)) target)
+  feedback5 = 0
 
 
 initialGuess::Int -> ([Card], GameState)
+initialGuess = undefined
 
 nextGuess:: ([Card], GameState)->(Int, Int, Int, Int, Int)-> ([Card], GameState)
+nextGuess = undefined
 
 data GameState = GameState {pastGuesses::[[Card]], pastFeedback::[(Int, Int, Int, Int, Int)]}
+type Feedback = (Int, Int, Int, Int, Int)
